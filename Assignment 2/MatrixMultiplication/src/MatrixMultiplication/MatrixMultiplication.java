db693c9d627e031b5661ef15eaa9f0c6945ca5af
rnibhriain <78806345+rnibhriain@users.noreply.github.com>
2024-03-25 11:40:56 +0000

    Setting up main

--- Assignment 2/MatrixMultiplication/src/MatrixMultiplication/MatrixMultiplication.java
+++ Assignment 2/MatrixMultiplication/src/MatrixMultiplication/MatrixMultiplication.java
@@ -1,10 +1,38 @@
 package MatrixMultiplication;
 
+import java.util.concurrent.*;
+
 public class MatrixMultiplication {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main( String [] args ) {		
+
+
+		//get number of CPUs available
+		int numCores = Runtime.getRuntime().availableProcessors();
+
+		System.out.println( "Number of cores available: " + numCores );
+		
+		// starting multicore fixed threads
+		final ExecutorService executor = Executors.newFixedThreadPool( numCores );
+		
+		executor.execute(() -> {
+
+			
+		});
+		
+		executor.shutdown();
+		
+		try {
+			if ( executor.awaitTermination( 1, TimeUnit.DAYS ) ) {
+			} else {
+			    executor.shutdownNow();
+			}
+		} catch ( InterruptedException ex ) {
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+		}
 
 	}
+
 
 }
