dfe964a2758258c008d41fbccb60d93ec4ccdb19
rnibhriain <78806345+rnibhriain@users.noreply.github.com>
2024-03-31 19:06:30 +0100

    Okay functions - not well - but it functions

--- Assignment 2/MatrixMult.cpp
+++ Assignment 2/MatrixMult.cpp
@@ -1,23 +1,18 @@
-#include <iostream>
-#include <vector>
-#include <chrono>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
+#include <sys/time.h>
 
-using namespace std;
-const int N = 1000;
-int main()
+
+int main ()
 {
-    vector<vector<int>> A(N, vector<int>(N));
-    vector<vector<int>> B(N, vector<int>(N));
-    vector<vector<int>> C(N, vector<int>(N));
 
     // Initialize matrices A and B with random values
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            A[i][j] = rand() % 100;
-            B[i][j] = rand() % 100;
         }
     }
 
-    cout << "Done!\n";
+    printf( "Done!\n" );
 }
