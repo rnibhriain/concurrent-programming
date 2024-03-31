0fd472a1559fd804dfd8df0debc841535fee6ee6
rnibhriain <78806345+rnibhriain@users.noreply.github.com>
2024-03-31 18:39:25 +0100

    Start OpenMP version

---
+++ Assignment 2/MatrixMult.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <omp.h>
+
+using namespace std;
+const int N = 1000;
+int main()
+{
+    vector<vector<int>> A(N, vector<int>(N));
+    vector<vector<int>> B(N, vector<int>(N));
+    vector<vector<int>> C(N, vector<int>(N));
+
+    // Initialize matrices A and B with random values
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            A[i][j] = rand() % 100;
+            B[i][j] = rand() % 100;
+        }
+    }
+
+    cout << "Done!\n";
+}
