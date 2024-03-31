a42bf3f7d63392a9ed9db5d58bbb8a2200e0c987
rnibhriain <78806345+rnibhriain@users.noreply.github.com>
2024-03-31 20:41:02 +0100

    I think this is a multiplier

--- Assignment 2/MatrixMult.c
+++ Assignment 2/MatrixMult.c
@@ -4,16 +4,44 @@
 #include <omp.h>
 #include <sys/time.h>
 
+#define N 16
+
+int A[N][N];
+int B[N][N];
+int C[N][N];
 
 int main ( int argc, char * argv [ ] )
 {
+    int i,j,k;
 
     int nthreads = atoi( argv[ 1 ] );
-    
-    #pragma omp parallel num_threads ( nthreads )
-    printf( "Hello from thread %d of %d \n", omp_get_thread_num( ), omp_get_num_threads( ) ) ;
+
+    omp_set_num_threads(omp_get_num_procs());
+    for (i= 0; i< N; i++)
+        for (j= 0; j< N; j++)
+	{
+            A[i][j] = 2;
+            B[i][j] = 2;
+	}
+
+
+    #pragma omp parallel for private(i,j,k) shared(A,B,C)
+    for (i = 0; i < N; ++i) {
+        for (j = 0; j < N; ++j) {
+            for (k = 0; k < N; ++k) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
 
     printf( "Done!\n" );
+
+    for ( i = 0; i < N; i++ ) {
+        for ( j = 0; j < N; j++ ) {
+            printf("[ %d ]", C[ i ][ j ] );
+        }
+        printf( "\n" );
+    }
 
     return 0; 
 }
