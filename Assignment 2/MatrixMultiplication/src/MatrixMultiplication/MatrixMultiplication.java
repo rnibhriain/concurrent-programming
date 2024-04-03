91c5d1fa6e61e860e5336093926826da5a6515b2
rnibhriain <78806345+rnibhriain@users.noreply.github.com>
2024-04-03 17:29:34 +0100

    Okay so obviously less threads

--- Assignment 2/MatrixMultiplication/src/MatrixMultiplication/MatrixMultiplication.java
+++ Assignment 2/MatrixMultiplication/src/MatrixMultiplication/MatrixMultiplication.java
@@ -5,11 +5,29 @@ import java.util.concurrent.*;
 public class MatrixMultiplication {
 
 	static int MAX_THREADS = 2;
-	static final int N = 256;
+	static final int N = 4096;
 	static int [][] matA = new int[N][N];
 	static int [][] matB = new int[N][N];
 	static int[][] matC = new int[N][N];
 
+	static void printMat ( int mat [][] ) {
+		int i, j; 
+
+		for ( i = 0; i < N; i++ ) {
+			for ( j = 0; j < N; j++ ) {
+				System.out.print( mat[ i ][ j ] + " " );
+			}
+			System.out.print( "\n" );
+		}
+	}
+
+	static void generateMat ( int mat [] [] ) {
+		for ( int i = 0; i < N; i++ ) {
+			for ( int j = 0; j < N; j++ ) {
+				mat[ i ][ j ] = (int) ( Math.random() * 10 );
+			}
+		}
+	}
 
 	static class Multiplier implements Runnable {
 		int i;
@@ -19,16 +37,24 @@ public class MatrixMultiplication {
 		}
 
 		public void run () {
-			for ( int j = 0; j < 2; j++ ) {
-				for ( int k = 0; k < 2; k++ ) {
+			for ( int j = 0; j < N; j++ ) {
+				for ( int k = 0; k < N; k++ ) {
 					matC[ i ][ j ] += matA[ i ][ k ] * matB[ k ][ j ];
 				}
 			}
 		}
 	}
 
-	public static void main( String [] args ) {		
+	public static void main( String [] args ) {	
 
+		generateMat( matA );
+
+		generateMat( matB );
+
+		System.out.print( "\nMatrix A\n" );
+		//printMat( matA );
+		System.out.print( "\nMatrix B\n" );
+		//printMat( matB );
 
 		//get number of CPUs available
 		int numCores = Runtime.getRuntime().availableProcessors();
@@ -38,9 +64,10 @@ public class MatrixMultiplication {
 		// starting multicore fixed threads
 		final ExecutorService executor = Executors.newFixedThreadPool( numCores );
 
-		
+		long startTime = System.currentTimeMillis();
+
 		executor.execute(() -> {
-			for ( int i = 0; i < MAX_THREADS; i++ ) {
+			for ( int i = 0; i < N; i++ ) {
 				Thread multiplier = new Thread( new Multiplier( i ) );
 				multiplier.start();
 			}
@@ -48,10 +75,16 @@ public class MatrixMultiplication {
 		});
 
 		executor.shutdown();
-
+		
 		try {
 			if ( executor.awaitTermination( 1, TimeUnit.DAYS ) ) {
 
+				long endTime = System.currentTimeMillis();
+
+				System.out.println("That took " + (endTime - startTime) + " milliseconds");
+				
+				//System.out.println("Contents of result matrix");
+				//printMat( matC );
 			} else {
 				executor.shutdownNow();
 			}
@@ -59,11 +92,6 @@ public class MatrixMultiplication {
 			ex.printStackTrace();
 		}
 
-		System.out.println("Contents of result matrix");
-		for(int i = 0; i < 2; i++) {
-			System.out.println("[" + matC[ i ][ 0 ] +","+ matC[ i ][ 1 ] +"]" );
-
-		}
 	}
 
 
